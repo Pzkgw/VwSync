060f94246ebd40f1855aa171f8623440edc32277
Visoiu Bogdan <panzerfaust2006@yahoo.com>
2016-11-29 18:05:38 +0200

    Add files via upload

---
+++ VwSyncSever/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VwSyncSever
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            Orchestrate();
+        }
+
+        private void Orchestrate()
+        {
+
+            // SETTINGS
+            string
+                firstLocation = @"\\10.10.10.47\video\gi test\demo\",
+                secondLocation = @"c:\\_\";
+
+
+            // FORM UPDATE
+
+            textBox2.Text = firstLocation;
+            atextBox2.Text = secondLocation;
+
+
+            //ORCHESTRATE-IT
+            Orchestrator o = new VwSyncSever.Orchestrator();
+            o.SetInfoShowPodium(infoLbl);
+            o.Start(firstLocation, secondLocation);
+            o.DoEeet();
+        }
+
+        private void btnSaveEditedText_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ VwSyncSever/Orchestrator.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using System.Windows.Controls;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+
+namespace VwSyncSever
+{
+    class Orchestrator
+    {
+
+        SyncOrchestrator orchestrator;
+        
+        public Orchestrator()
+        {
+            
+        }
+
+
+        
+
+        public void Start(string localProvider, string remoteProvider)
+        {
+
+            if (Directory.Exists(localProvider) && Directory.Exists(remoteProvider))
+            {
+                FileSyncProvider firstProvider = new FileSyncProvider(localProvider);
+                FileSyncProvider secondProvider = new FileSyncProvider(remoteProvider);
+
+                orchestrator = new SyncOrchestrator();
+                orchestrator.LocalProvider = firstProvider;
+                orchestrator.RemoteProvider = secondProvider;
+                orchestrator.Direction = SyncDirectionOrder.DownloadAndUpload;
+            }
+            else
+            {
+                Show("Dir !exists");
+            }
+
+
+            
+
+        }
+
+
+        public void DoEeet()
+        {
+            if (orchestrator != null) {
+                orchestrator.Synchronize();
+            }
+            else
+            {
+                Show("orchestrator !exists");
+            }
+            
+        }
+
+
+
+        #region GUI_STUFF
+
+        Label s;
+        public void SetInfoShowPodium(Label inf)
+        {
+            s = inf;
+        }
+
+
+        private void Show(string mesaj)
+        {
+            s.Content = mesaj;
+        }
+
+
+        #endregion
+
+    }
+}
